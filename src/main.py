76992ec19db8bb42b3b122f55c7ee4afc728b7fb
ButurcaDragos-mky <dragobeast251@gmail.com>
2026-01-18 15:36:12 +0200

    Added Clamp and Steering fucntions

--- src/main.py
+++ src/main.py
@@ -27,13 +27,22 @@ def random_velocity(max_speed: float) -> pygame.Vector2:
     #- Then multiply it by a random speed (between ~30% and 100% of max_speed).
     
     v = pygame.Vector2(random.uniform(-1, 1), random.uniform(-1, 1))
-    #Edge Case if = 0
+    # Edge Case if = 0
     if v.length_squared() == 0:
         v = pygame.Vector2(1, 0)
-     # not all dots move at the same speed
+     # Not all dots move at the same speed
     v = v.normalize() * random.uniform(max_speed * 0.3, max_speed)
     return v
 
+def clamp_length(v: pygame.Vector2, max_len: float) -> pygame.Vector2:
+    if v.length_squared() > max_len * max_len:
+        return v.normalize() * max_len
+    return v
+
+def steer_towards(current_vel: pygame.Vector2, desired_vel: pygame.Vector2, max_force: float) -> pygame.Vector2:
+    # Steering
+    steer = desired_vel - current_vel
+    return clamp_length(steer, max_force)
 
 def bounce_off_walls(pos: pygame.Vector2, vel: pygame.Vector2, w: int, h: int, r: int):
     # Left wall
@@ -69,17 +78,17 @@ def main():
         vel = random_velocity(MAX_SPEED)
         #store dots case
         dots.append({"pos": pos, "vel": vel})
-    #Main Loop
+    # Main Loop
     running = True
     while running:
         # clock.tick(60) tries to cap the loop at ~60 FPS.
         dt = clock.tick(60) / 1000.0  
-        #Event Handling
+        # Event Handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        #Physics game
+        # Physics game
         for d in dots:
             d["pos"] += d["vel"] * dt
             bounce_off_walls(d["pos"], d["vel"], WIDTH, HEIGHT, DOT_RADIUS)
@@ -102,9 +111,9 @@ def main():
             d["vel"].y *= -1  
 
 
-        #Render game
+        # Render game
         screen.fill(BG)
-        #Draw dots
+        # Draw dots
         for d in dots:
             pygame.draw.circle(screen, DOT, (int(d["pos"].x), int(d["pos"].y)), DOT_RADIUS)
         pygame.display.flip()
